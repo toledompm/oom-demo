02df97ea52cc6542a9434d136b49b4a20af30faa
marcos.toledo <marcos.toledo@wildlifestudios.com>
2024-08-15 11:29:57 -0300

    try to move memory into the heap

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"time"
 )
 
+var chunks [][]byte
+var totalUsedBytes int = 0
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: main <command>")
@@ -19,7 +22,17 @@ func main() {
 	case "overcommit":
 		overcommit()
 	case "oom":
-		oom()
+		go func() {
+			oom()
+		}()
+		for {
+			time.Sleep(250 * time.Millisecond)
+			fmt.Printf("Used %.2f GB\n", float64(totalUsedBytes)/1024/1024/1024)
+
+			var m runtime.MemStats
+			runtime.ReadMemStats(&m)
+			fmt.Printf("HeapAlloc: %v bytes, Sys: %v bytes\n", m.HeapAlloc, m.Sys)
+		}
 	default:
 		fmt.Println("Invalid command")
 		os.Exit(1)
@@ -27,9 +40,6 @@ func main() {
 }
 
 func oom() {
-	var chunks [][]byte
-	var totalUsedBytes int = 0
-
 	malloc_size := 1024 * 1024 * 256
 	chunk := make([]byte, malloc_size)
 
@@ -43,11 +53,6 @@ func oom() {
 		chunks = append(chunks, tmp)
 
 		totalUsedBytes += malloc_size
-		fmt.Printf("Used %.2f GB\n", float64(totalUsedBytes)/1024/1024/1024)
-
-		var m runtime.MemStats
-		runtime.ReadMemStats(&m)
-		fmt.Printf("HeapAlloc: %v bytes, Sys: %v bytes\n", m.HeapAlloc, m.Sys)
 	}
 
 }
